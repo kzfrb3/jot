326d8a90636abd973371f26725e949ff4ea698cf
Thomas Yager-Madden <10351894+tym-xqo@users.noreply.github.com>
2019-09-24 10:36:22 -0500

    include style in template and build

--- jot.py
+++ jot.py
@@ -1,3 +1,4 @@
+import shutil
 from pathlib import Path
 
 import pytz
@@ -27,34 +28,45 @@ def sort_posts():
     utc = pytz.timezone("UTC")
     for post in posts:
         post = parse_post(post)
-        post["metadata"]["date"] = utc.localize(post["metadata"]["date"]).isoformat()
+        post["metadata"]["stamp"] = utc.localize(post["metadata"]["stamp"]).isoformat()
         sorted_posts.append(post)
     all_sorted = sorted(
-        sorted_posts, key=lambda post: post["metadata"]["date"], reverse=True
+        sorted_posts, key=lambda post: post["metadata"]["stamp"], reverse=True
     )
     return all_sorted
 
 
 def render(posts=[]):
+    if not posts:
+        posts = sort_posts()
     with open("template.j2") as template_file:
         template = Template(template_file.read())
-    html = template.render(posts)
+    html = template.render(posts=posts)
     return html
 
 
+def copy_css():
+    src = Path("style.css")
+    dest = Path("site_build/style.css")
+    shutil.copy(src, dest)
+
+
 def build():
+    """
+    """
     Path("site_build").mkdir(exist_ok=True)
+    copy_css()
     posts = sort_posts()
     html = render(posts=posts)
     with open("site_build/index.html", "w") as output:
         output.write(html)
-    # for post in sort_posts():
-    #     title = post["metadata"]["title"]
-    #     filename = f"site_build/{title}.html"
-    #     with open(filename, "w") as post_output:
-    #         html = render(posts=[post])
-    #         post_output.write(html)
+    for post in sort_posts():
+        slug = post["metadata"]["slug"]
+        filename = f"site_build/{slug}.html"
+        with open(filename, "w") as post_output:
+            html = render(posts=[post])
+            post_output.write(html)
 
 
 if __name__ == "__main__":
-    print(render(posts=sort_posts()))
+    build()
--- posts.md
+++ posts.md
@@ -1,26 +1,26 @@
 ---
-date: 2019-09-23T15:30:13Z 
-title: "53a1559"
+stamp: 2019-09-23T15:30:13Z 
+slug: "53a1559"
 ---
 Stupid blog is stupid. [Eleventy](https://www.11ty.io) is kinda neat, tho.
 
 {end}
 ---
-title: b0e5d45
-date: 2019-09-23T19:55:41Z
+slug: b0e5d45
+stamp: 2019-09-23T19:55:41Z
 ---
 I think what I'd really like is to be able to keep all the posts in a single file, that I can just keep appending to. The build script should be able to parse the individual posts out. I guess I could probably find a way to do this in JS and plug it in to 11ty, but can't help wondering if it wouldn't be easier to roll my own in python.
 
 {end}
 ---
-title: cbce371
-date: 2019-09-23T18:15:02Z
+slug: cbce371
+stamp: 2019-09-23T18:15:02Z
 ---
 Really not sure yet what I'm doing with this, except that I want it to be really lightweight, quick, easy blogging for short posts. A "microblog" without any "social" aspects. Just an old-fashioned blog, if you will.
 
 {end}
 ---
-date: 2019-09-23T16:35:11Z
-title: "ef2eb1b"
+stamp: 2019-09-23T16:35:11Z
+slug: "ef2eb1b"
 ---
 And but so I was getting some weird failed builds trying to build from gitlab push. Let's see how this one goes (if you're reading this, it worked!)
